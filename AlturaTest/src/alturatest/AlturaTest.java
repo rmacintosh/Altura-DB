2af2999a0c647ad6ba3940bce3cd13c5af6bbeb5
rmacintosh <rmacintosh@gmail.com>
2014-03-30 09:54:02 -0600

    Move main function to alturatest
    Moved main function to the alturatest class and fixed the look and feel
    settings

--- AlturaTest/src/alturatest/AlturaTest.java
+++ AlturaTest/src/alturatest/AlturaTest.java
@@ -15,8 +15,40 @@ public class AlturaTest {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+                    //javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    //String laf = javax.swing.UIManager.getSystemLookAndFeelClassName();
+                    //System.out.println(laf);
+                    //System.out.println(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AlturaJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AlturaJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AlturaJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AlturaJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AlturaJFrame().setVisible(true);
+            }
+        });
     }
     
     public static void newTerminal(javax.swing.JTextField serialNum, javax.swing.JTextField pimsNum, javax.swing.JTextField location, int test4, javax.swing.JTextField condition) {
